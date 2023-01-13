2108d083b9e32bc2f1f35092682b981b3b63db1b
OpportunityLiu <opportunity@live.in>
2023-01-13 19:03:45 +0800

    add shell options

--- src/natmap.js
+++ src/natmap.js
@@ -1,17 +1,17 @@
 import { spawn } from "node:child_process";
 
+const EXEC = "natmap";
+const STUN_SERVER = "stun.stunprotocol.org";
+const HOLD_SERVER = "qq.com";
+
+let port = 9001;
+
 export function start(sourceIp, sourcePort, udpMode) {
-  let natmap;
-  if (udpMode) {
-    natmap = spawn(
-      `natmap -u -s stun.stunprotocol.org -b 0 -t ${sourceIp} -p ${sourcePort}`,
-      { stdio: "pipe" }
-    );
-  } else {
-    natmap = spawn(
-      `natmap -s stun.stunprotocol.org -h qq.com -b 0 -t ${sourceIp} -p ${sourcePort}`,
-      { stdio: "pipe" }
-    );
-  }
+  const natmap = spawn(
+    `${EXEC} -s ${STUN_SERVER} -h ${HOLD_SERVER} -b ${port} -t ${sourceIp} -p ${sourcePort} ${
+      udpMode ? "-u" : ""
+    }`,
+    { shell: true }
+  );
   natmap.stdout.on("data", (ch) => console.log(ch));
 }
