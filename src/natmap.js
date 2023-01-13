acebfa06c1dc50fb44ff50efa8081ad8c67ffa90
OpportunityLiu <opportunity@live.in>
2023-01-13 19:52:33 +0800

    fix

--- src/natmap.js
+++ src/natmap.js
@@ -1,5 +1,5 @@
 import { spawn } from "node:child_process";
-import { updatePublicIp } from "./public-ip";
+import { updatePublicIp } from "./public-ip.js";
 
 const EXEC = "natmap";
 const STUN_SERVER = "stun.stunprotocol.org";
